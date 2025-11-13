2975341108b2239343d461e9b0e915a062628912
Alfred de Vulpian <derfladv@gmail.com>
2025-11-13 13:19:03 +0100

    feat: replace boilerplate informations with mine

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,20 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Alfred de Vulpian — Etudiant en Data & IA à l'ECE Paris",
+  author: "Alfred de Vulpian",
   description:
-    "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
+    "Etudiant en Master 1 à l'ECE Paris, je me spécialise en Data Science et Intelligence Artificielle.",
   lang: "en",
-  siteLogo: "/alejandro-small.jpg",
+  siteLogo: "/alfred-small.png",
   navLinks: [
     { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
-    { text: "Twitter", href: "https://github.com/immois/astro-zen" },
-    { text: "LinkedIn", href: "https://github.com/immois/astro-zen" },
-    { text: "Github", href: "https://github.com/immois/astro-zen" },
-    { text: "Youtube", href: "https://github.com/immois/astro-zen" },
-    { text: "Dribbble", href: "https://github.com/immois/astro-zen" },
+    { text: "LinkedIn", href: "https://linkedin.com/in/alfred-de-vulpian" },
+    { text: "Github", href: "https://github.com/alfred0404" },
   ],
   socialImage: "/zen-og.png",
   canonicalURL: "https://astro-zen.vercel.app",
@@ -25,73 +22,76 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Alfred de Vulpian",
+    specialty: "Etudiant en Data & IA à l'ECE Paris",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+      "Etudiant en Master 1 à l'ECE Paris, je me spécialise en Data Science et Intelligence Artificielle. Je suis actuellement à la recherche d'un stage technique dans ces domaines à partir d'avril 2026 pour 4 mois.",
+    email: "alfred.devulpian@edu.ece.fr",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      company: "Crédit Agricole Assurances",
+      position: "Data Scientist (Stage)",
+      startDate: "Janvier 2025",
+      endDate: "Fevrier 2025",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "Participation au développement en Python d'un algorithme de préparation de données pour l'entraînement de modèles (+5% de précision).",
+        "Annotation de documents pour l'entraînement de modèles d'extraction de texte.",
       ],
     },
     {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
+      company: "Havas Data Business Intelligence",
+      position: "Data Scientist (Stage)",
+      startDate: "Janvier 2024",
+      endDate: "Février 2024",
       summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
+        "Développement en Python d'une pipeline ETL vers une datawarehouse BigQuery.",
+        "Extraction grâce à Python (pandas) des données du parcours utilisateur sur le site des clients permettant leur analyse et l'optimisation du taux de transformation",
       ],
-    },
-    {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
     },
   ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
+      name: "Rag Pipeline",
+      summary:
+        "Un chatbot Retrieval-Augmented Generation (RAG) construit avec Langchain, Ollama (mistral:7b) et Chroma pour le stockage vectoriel.",
       linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/spotifu.png",
+      linkSource: "https://github.com/alfred0404/rag_chatbot",
+      image: "/rag_chatbot.png",
     },
     {
-      name: "Shopp App",
-      summary: "An e-commerce platform that replicates Shopify's key features.",
+      name: "Genetic Algorithm for AI Race Cars",
+      summary:
+        "Une simulation de voitures de course IA utilisant des algorithmes génétiques pour faire évoluer les comportements des véhicules au fil des générations.",
       linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/shopify-clon.png",
+      linkSource: "https://github.com/alfred0404/genetic_ai_racer",
+      image: "/genetic_ai_racer.png",
     },
     {
-      name: "ClonTagram",
-      summary: "A social network that replicates the features of Instagram",
+      name: "GAN",
+      summary:
+        "Implémentation complète de Generative Adversarial Network (GAN) en utilisant PyTorch.",
       linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/clone-ig.png",
+      linkSource: "https://github.com/alfred0404/gan",
+      image: "/gan.png",
+    },
+    {
+      name: "BPE Tokenizer",
+      summary: "Implémentation d'un tokenizer Byte Pair Encoding (BPE).",
+      linkPreview: "/",
+      linkSource: "https://github.com/alfred0404/bpe_tokenizer",
+      image: "/bpe_tokenizer.jpg",
     },
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
+      Bonjour, je suis Alfred de Vulpian, étudiant en 4ᵉ année à l'ECE Paris, spécialisé en data science et intelligence artificielle. Passionné par la technologie et l'innovation, je développe régulièrement des projets personnels mêlant Python, IA et analyse de données.
 
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+      J'ai construit une solide maîtrise de Python et de ses principaux écosystèmes — du machine learning à l'automatisation, en passant par le développement d'outils et d'applications intelligentes. J'aime comprendre en profondeur ce que je crée, alliant rigueur technique et curiosité scientifique.
+
+      Mon objectif : concevoir des solutions claires, performantes et utiles, en explorant le potentiel de l'intelligence artificielle pour répondre à des problématiques réelles et concrètes.
     `,
-    image: "/alejandro-big.jpg",
+    image: "/alfred-big.png",
   },
 };
 
